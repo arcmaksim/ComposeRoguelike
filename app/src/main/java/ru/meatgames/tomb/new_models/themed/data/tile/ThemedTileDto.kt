3882f2c95c3b311061e2f78eb106cd605aed74df
Maxim Artsybasov <arcmaksim@gmail.com>
2022-09-10 00:19:14 +0300

    Update tile definition naming

--- app/src/main/java/ru/meatgames/tomb/new_models/themed/data/tile/ThemedTileDto.kt
+++
@@ -1,9 +0,0 @@
-package ru.meatgames.tomb.new_models.themed.data.tile
-
-import ru.meatgames.tomb.new_models.themed.data.ThemedTilePurposeDto
-
-@kotlinx.serialization.Serializable
-class ThemedTileDto(
-    val purpose: ThemedTilePurposeDto,
-    val horizontalTileOffset: Int,
-)
