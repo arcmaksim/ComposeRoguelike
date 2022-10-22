50b213f14f4b2fdfb83667b07f35d6fffd68cd38
arcmaksim <arcmaksim@gmail.com>
2022-10-22 23:09:29 +0200

    Update DI

--- app/src/main/java/ru/meatgames/tomb/di/SingletonModule.kt
+++ app/src/main/java/ru/meatgames/tomb/di/SingletonModule.kt
@@ -1,15 +1,27 @@
 package ru.meatgames.tomb.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ru.meatgames.tomb.domain.MapController
+import ru.meatgames.tomb.domain.MapControllerImpl
+import ru.meatgames.tomb.domain.MapGenerator
+import ru.meatgames.tomb.domain.MapTerraformer
+import ru.meatgames.tomb.new_models.themed.data.ThemedRoomsData
+import ru.meatgames.tomb.new_models.themed.data.ThemedRoomsRepository
 import javax.inject.Named
 
 private const val MAP_WIDTH = 32
 const val MAP_WIDTH_KEY = "MAP_WIDTH"
 private const val MAP_HEIGHT = 32
 const val MAP_HEIGHT_KEY = "MAP_HEIGHT"
+
+private const val MAP_VIEWPORT_WIDTH = 11
+const val MAP_VIEWPORT_WIDTH_KEY = "MAP_VIEWPORT_WIDTH"
+private const val MAP_VIEWPORT_HEIGHT = 11
+const val MAP_VIEWPORT_HEIGHT_KEY = "MAP_VIEWPORT_HEIGHT"
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -22,5 +34,39 @@ class SingletonModule {
     @Named(MAP_HEIGHT_KEY)
     @Provides
     fun mapHeightConst(): Int = MAP_HEIGHT
+
+    @Named(MAP_VIEWPORT_WIDTH_KEY)
+    @Provides
+    fun mapViewportWidthConst(): Int = MAP_VIEWPORT_WIDTH
+
+    @Named(MAP_VIEWPORT_HEIGHT_KEY)
+    @Provides
+    fun mapViewportHeightConst(): Int = MAP_VIEWPORT_HEIGHT
+
+    @Provides
+    fun provideThemedRoomsData(
+        themedRoomsRepository: ThemedRoomsRepository,
+    ): ThemedRoomsData = themedRoomsRepository.loadData()
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class BindingSingletonModule {
+
+    @Binds
+    abstract fun bindMapGenerator(
+        controller: MapControllerImpl,
+    ): MapGenerator
+
+    @Binds
+    abstract fun bindMapTerraformer(
+        controller: MapControllerImpl,
+    ): MapTerraformer
+
+    @Binds
+    abstract fun bindMapController(
+        controller: MapControllerImpl,
+    ): MapController
 
 }
