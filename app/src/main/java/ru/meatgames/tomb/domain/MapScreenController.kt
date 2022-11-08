a27e9cb019091f50f0b1dcda39d40999dc0828b2
arcmaksim <arcmaksim@gmail.com>
2022-11-08 21:41:29 +0100

    Update MapScreenController

--- app/src/main/java/ru/meatgames/tomb/domain/MapScreenController.kt
+++ app/src/main/java/ru/meatgames/tomb/domain/MapScreenController.kt
@@ -1,7 +1,5 @@
 package ru.meatgames.tomb.domain
 
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.unit.IntOffset
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -9,15 +7,11 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
-import ru.meatgames.tomb.NewAssets
 import ru.meatgames.tomb.di.MAP_VIEWPORT_HEIGHT_KEY
 import ru.meatgames.tomb.di.MAP_VIEWPORT_WIDTH_KEY
-import ru.meatgames.tomb.model.tile.domain.GeneralTilePurpose
-import ru.meatgames.tomb.model.tile.domain.Tile
-import ru.meatgames.tomb.model.tile.domain.TilePurposeDefinition
-import ru.meatgames.tomb.model.tile.domain.isEmpty
+import ru.meatgames.tomb.model.temp.TilesController
+import ru.meatgames.tomb.render.MapRenderTile
 import ru.meatgames.tomb.screen.compose.game.MapTile
-import ru.meatgames.tomb.screen.compose.game.resolveTileset
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -28,14 +22,18 @@ class MapScreenController @Inject constructor(
     private val viewportHeight: Int,
     private val mapController: MapController,
     private val characterController: CharacterController,
+    private val tilesController: TilesController,
+    private val mapRenderProcessor: MapRenderProcessor,
 ) {
 
     private val _state = MutableStateFlow<MapScreenState>(MapScreenState.Loading)
     val state: StateFlow<MapScreenState> = _state
 
-    private var cachedVisibilityMask = BooleanArray(0) { false }
+    private val cachedVisibilityMask = BooleanArray(viewportWidth * viewportWidth) { false }
 
-    private val purposeDefinitionOffsetCache = HashMap<TilePurposeDefinition, IntOffset>()
+    private val preProcessingBufferSizeModifier: Int = 1
+    private val preProcessingViewportWidth: Int = viewportWidth + 2 * preProcessingBufferSizeModifier
+    private val preProcessingViewportHeight: Int = viewportHeight + 2 * preProcessingBufferSizeModifier
 
     init {
         GlobalScope.launch {
@@ -70,21 +68,17 @@ class MapScreenController @Inject constructor(
         mapWidth: Int,
         mapHeight: Int,
     ): MapScreenState.Ready {
-        updateVisibilityMask(tiles)
+        cachedVisibilityMask.updateVisibilityMask()
 
-        val characterScreenX = viewportWidth / 2
-        val characterScreenY = viewportHeight / 2
-
-        val mapOffsetX = characterX - characterScreenX
-        val mapOffsetY = characterY - characterScreenY
-
-        val filteredTiles = filterTiles(
-            streamedTiles = tiles,
-            mapX = mapOffsetX,
-            mapY = mapOffsetY,
+        val filteredTiles = tiles.filterTiles(
+            mapX = characterX - preProcessingViewportWidth / 2,
+            mapY = characterY - preProcessingViewportHeight / 2,
             mapWidth = mapWidth,
             mapHeight = mapHeight,
         )
+
+        val characterScreenX = viewportWidth / 2
+        val characterScreenY = viewportHeight / 2
 
         computeFov(
             originX = characterScreenX,
@@ -92,148 +86,76 @@ class MapScreenController @Inject constructor(
             maxDepth = viewportWidth / 2 + 1,
             revealTile = { x, y -> cachedVisibilityMask[x + y * viewportWidth] = true },
             checkIfTileIsBlocking = { x, y ->
-                !filteredTiles[x + y * viewportWidth].isTransparent
+                val index = (x + 1) + (y + 1) * preProcessingViewportWidth
+                val objectEntity = filteredTiles[index]?.objectEntityTile ?: return@computeFov false
+                !tilesController.isObjectEntityVisibleThrough(
+                    objectEntity = objectEntity,
+                )
             }
         )
 
-        return produceReadyState(
+        return MapScreenState.Ready(
             viewportWidth = viewportWidth,
             viewportHeight = viewportHeight,
-            tiles = filteredTiles,
+            tiles = mapRenderProcessor.produceRenderTilesFrom(
+                tiles = filteredTiles,
+                tilesLineWidth = preProcessingViewportWidth,
+                shouldRenderTile = { index ->
+                    cachedVisibilityMask[index]
+                },
+            ),
         )
     }
 
-    private fun updateVisibilityMask(
-        tiles: List<MapTile>,
-    ) {
-        if (cachedVisibilityMask.size != tiles.size) {
-            cachedVisibilityMask = BooleanArray(tiles.size) { false }
-        } else {
-            cachedVisibilityMask.fill(false)
-        }
+    private fun BooleanArray.updateVisibilityMask() {
+        fill(false)
     }
 
-    private fun filterTiles(
-        streamedTiles: List<MapTile>,
+    private fun List<MapTile>.filterTiles(
         mapX: Int,
         mapY: Int,
         mapWidth: Int,
         mapHeight: Int,
-    ): List<MapTile> = (0 until viewportHeight).map { line ->
+    ): List<MapTile?> = (0 until preProcessingViewportHeight).map { line ->
         val start = (mapY + line) * mapWidth + mapX
-        val end = start + viewportWidth
+        val end = start + preProcessingViewportWidth
         when {
             mapY + line !in 0 until mapHeight -> {
-                List(viewportWidth) { MapTile.voidMapTile }
+                List(preProcessingViewportWidth) { null }
             }
 
             mapX < 0 -> {
-                List(viewportWidth) {
+                List(preProcessingViewportWidth) {
                     when {
-                        mapX + it < 0 -> MapTile.voidMapTile
-                        else -> streamedTiles[start + it]
+                        mapX + it < 0 -> null
+                        else -> this[start + it]
                     }
                 }
             }
 
-            mapX + viewportWidth > mapWidth -> {
-                List(
-                    viewportWidth
-                ) {
+            mapX + preProcessingViewportWidth > mapWidth -> {
+                List(preProcessingViewportWidth) {
                     when {
-                        mapX + it < mapWidth -> streamedTiles[start + it]
-                        else -> MapTile.voidMapTile
+                        mapX + it < mapWidth -> this[start + it]
+                        else -> null
                     }
                 }
             }
 
-            else -> streamedTiles.subList(start, end)
+            else -> this.subList(start, end)
         }
     }.fold(emptyList()) { acc, item -> acc + item }
-
-    private fun produceReadyState(
-        viewportWidth: Int,
-        viewportHeight: Int,
-        tiles: List<MapTile>,
-    ): MapScreenState.Ready = MapScreenState.Ready(
-        viewportWidth = viewportWidth,
-        viewportHeight = viewportHeight,
-        tiles = tiles.mapIndexed { index, tile ->
-            if (!cachedVisibilityMask[index]) return@mapIndexed null
-
-            val floorTile = tile.floor?.let {
-                MapScreenTile(
-                    asset = it.purposeDefinition.resolveTileset(),
-                    srcOffset = it.getOffset(),
-                )
-            } ?: MapScreenTile(
-                asset = NewAssets.tileset,
-                srcOffset = IntOffset(
-                    x = NewAssets.tileSize.width * 2,
-                    y = NewAssets.tileSize.height * 1,
-                ),
-            )
-
-            val objectTile = tile.`object`?.let {
-                if (it.purposeDefinition.isEmpty) {
-                    null
-                } else {
-                    MapScreenTile(
-                        asset = it.purposeDefinition.resolveTileset(),
-                        srcOffset = it.getOffset(),
-                    )
-                }
-            }
-
-            MapScreenCell(
-                floorTile = floorTile,
-                objectTile = objectTile,
-            )
-        },
-    )
-
-    private fun Tile.getOffset(): IntOffset = purposeDefinitionOffsetCache.getOrPut(
-        purposeDefinition,
-    ) {
-        val tileSize = NewAssets.tileSize.width
-        when (purposeDefinition) {
-            is TilePurposeDefinition.Standard -> IntOffset(
-                x = purposeDefinition.horizontalTileOffset * tileSize,
-                y = theme.verticalTileOffset * tileSize,
-            )
-            is TilePurposeDefinition.General -> {
-                val (offsetX, offsetY) = when (purposeDefinition.purpose) {
-                    GeneralTilePurpose.ClosedDoor -> 3 to 0
-                    GeneralTilePurpose.OpenDoor -> 1 to 1
-                }
-                IntOffset(
-                    x = offsetX * tileSize,
-                    y = offsetY * tileSize,
-                )
-            }
-        }
-    }
 
     sealed class MapScreenState {
 
         data class Ready(
             val viewportWidth: Int,
             val viewportHeight: Int,
-            val tiles: List<MapScreenCell?>,
+            val tiles: List<MapRenderTile?>,
         ) : MapScreenState()
 
         object Loading : MapScreenState()
 
     }
-
-    data class MapScreenTile(
-        val asset: ImageBitmap,
-        val srcOffset: IntOffset,
-    )
-
-    data class MapScreenCell(
-        val floorTile: MapScreenTile,
-        val objectTile: MapScreenTile?,
-    )
 
 }
