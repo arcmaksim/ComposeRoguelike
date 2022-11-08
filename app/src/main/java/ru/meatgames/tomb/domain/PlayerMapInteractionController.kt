b1a5d9414481a5f9045c2f04be5ab29b08935b22
arcmaksim <arcmaksim@gmail.com>
2022-11-08 21:01:46 +0100

    Update PlayerMapInteractionController

--- app/src/main/java/ru/meatgames/tomb/domain/PlayerMapInteractionController.kt
+++ app/src/main/java/ru/meatgames/tomb/domain/PlayerMapInteractionController.kt
@@ -3,18 +3,16 @@ package ru.meatgames.tomb.domain
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import ru.meatgames.tomb.Direction
-import ru.meatgames.tomb.model.tile.domain.Tile
-import ru.meatgames.tomb.model.tile.domain.TilePurposeDefinition
-import ru.meatgames.tomb.model.tile.domain.toTile
-import ru.meatgames.tomb.model.tile.domain.GeneralTilePurpose
+import ru.meatgames.tomb.model.temp.TilesController
+import ru.meatgames.tomb.model.tile.domain.ObjectEntityTile
 import ru.meatgames.tomb.resolvedOffsets
-import ru.meatgames.tomb.screen.compose.game.MapTile
 import javax.inject.Inject
 
 class PlayerMapInteractionController @Inject constructor(
     private val characterController: CharacterController,
     private val mapTerraformer: MapTerraformer,
     private val mapController: MapController,
+    private val tilesController: TilesController,
 ) {
 
     private val characterStateFlow = characterController.characterStateFlow
@@ -40,43 +38,42 @@ class PlayerMapInteractionController @Inject constructor(
         ) ?: return
 
         when {
-            tile.`object`?.isUsable == true -> {
+            tile.objectEntityTile?.let(tilesController::hasObjectEntityInteraction) == true -> {
                 useTile(
                     mapX = mapX,
                     mapY = mapY,
-                    tile = tile,
+                    objectEntityTile = tile.objectEntityTile,
                 )
             }
 
-            tile.`object` == null || tile.`object`.isPassable -> {
+            tile.objectEntityTile?.let(tilesController::hasObjectEntityNoInteraction) == true -> {
+                characterController.move(direction)
+            }
+
+            tile.objectEntityTile == null -> {
                 characterController.move(direction)
             }
         }
     }
 
+    // will be used by AI?
     fun useTile(
         mapX: Int,
         mapY: Int,
-        tile: MapTile,
+        objectEntityTile: ObjectEntityTile,
     ) {
-        val resolvedTileReplacementOnUse = tile.`object`?.resolveTileReplacementOnUse() ?: return
+        val resolvedTileReplacementOnUse = objectEntityTile.resolveTileReplacementOnUse() ?: return
 
         mapTerraformer.changeObject(
             x = mapX,
             y = mapY,
-            objectTile = resolvedTileReplacementOnUse,
+            objectEntityTile = resolvedTileReplacementOnUse,
         )
     }
 
-    private fun Tile.resolveTileReplacementOnUse(): Tile? {
-        val definition = purposeDefinition as? TilePurposeDefinition.General ?: return null
-        return when (definition.purpose) {
-            GeneralTilePurpose.ClosedDoor -> ru.meatgames.tomb.model.tile.domain.TilePurposeDefinition.General(
-                purpose = GeneralTilePurpose.OpenDoor,
-            ).toTile(theme)
-
-            else -> null
-        }
+    private fun ObjectEntityTile.resolveTileReplacementOnUse(): ObjectEntityTile? = when (this) {
+        ObjectEntityTile.DoorClosed -> ObjectEntityTile.DoorOpened
+        else -> null
     }
 
 }
