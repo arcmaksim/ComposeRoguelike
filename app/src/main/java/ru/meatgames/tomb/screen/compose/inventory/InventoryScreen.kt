e13f95514a36790950a99738c3a101332d7559bc
Maksim Artsybasov <maksim.artsybasov@glovoapp.com>
2023-02-19 23:43:07 +0100

    Add Toolbar

--- app/src/main/java/ru/meatgames/tomb/screen/compose/inventory/InventoryScreen.kt
+++ app/src/main/java/ru/meatgames/tomb/screen/compose/inventory/InventoryScreen.kt
@@ -2,13 +2,7 @@ package ru.meatgames.tomb.screen.compose.inventory
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,14 +10,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import ru.meatgames.tomb.R
-import ru.meatgames.tomb.design.h2TextStyle
 import ru.meatgames.tomb.design.h3TextStyle
+import ru.meatgames.tomb.screen.compose.system.Toolbar
 
 @Preview(widthDp = 320, heightDp = 640, showBackground = true, backgroundColor = 0xFF212121)
 @Composable
@@ -64,29 +55,11 @@ fun InventoryScreenContent(
     Column(
         modifier = Modifier.fillMaxSize(),
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            IconButton(
-                modifier = Modifier.size(36.dp),
-                onClick = onBack,
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_arrow_back),
-                    contentDescription = "Back button",
-                    tint = Color.White,
-                )
-            }
-            Text(
-                modifier = Modifier.weight(1f, fill = true),
-                text = "Inventory",
-                textAlign = TextAlign.End,
-                style = h2TextStyle,
-            )
-        }
+        Toolbar(
+            title = "Inventory",
+            navigationIconResId = R.drawable.ic_arrow_back,
+            onNavigationIcon = onBack,
+        )
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center,
