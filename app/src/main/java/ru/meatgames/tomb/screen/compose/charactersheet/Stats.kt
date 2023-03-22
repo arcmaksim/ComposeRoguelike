867e8a4e082527ff0617e340001890758b6bf273
Maksim Artsybasov <maksim.artsybasov@glovoapp.com>
2023-03-22 23:13:19 +0100

    Update stats presentation

--- app/src/main/java/ru/meatgames/tomb/screen/compose/charactersheet/Stats.kt
+++ app/src/main/java/ru/meatgames/tomb/screen/compose/charactersheet/Stats.kt
@@ -1,18 +1,32 @@
 package ru.meatgames.tomb.screen.compose.charactersheet
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ru.meatgames.tomb.R
+import ru.meatgames.tomb.design.cunningColor
 import ru.meatgames.tomb.design.h1TextStyle
 import ru.meatgames.tomb.design.h3TextStyle
+import ru.meatgames.tomb.design.powerColor
+import ru.meatgames.tomb.design.speedColor
+import ru.meatgames.tomb.design.techniqueColor
 import ru.meatgames.tomb.domain.stat.Cunning
 import ru.meatgames.tomb.domain.stat.Power
 import ru.meatgames.tomb.domain.stat.Speed
@@ -37,48 +51,91 @@ internal fun Stats(
     technique: Technique,
 ) {
     Box(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .padding(vertical = 16.dp),
         contentAlignment = Alignment.CenterStart,
     ) {
         Column {
-            Stat(
-                statValue = power.value,
-                statName = "Power",
-            )
-            Stat(
-                statValue = speed.value,
-                statName = "Speed",
-            )
-            Stat(
-                statValue = cunning.value,
-                statName = "Cunning",
-            )
-            Stat(
-                statValue = technique.value,
-                statName = "Technique",
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Stat(
+                    iconResId = R.drawable.ic_power,
+                    statValue = power.value,
+                    statName = "Power",
+                    color = powerColor,
+                )
+                Stat(
+                    iconResId = R.drawable.ic_cunning,
+                    statValue = cunning.value,
+                    statName = "Cunning",
+                    color = cunningColor,
+                )
+            }
+            
+            Spacer(modifier = Modifier.height(16.dp))
+    
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Stat(
+                    iconResId = R.drawable.ic_speed,
+                    statValue = speed.value,
+                    statName = "Speed",
+                    color = speedColor,
+                )
+                Stat(
+                    iconResId = R.drawable.ic_technique,
+                    statValue = technique.value,
+                    statName = "Technique",
+                    color = techniqueColor,
+                )
+            }
         }
     }
 }
 
 @Composable
 private fun Stat(
+    modifier: Modifier = Modifier,
+    @DrawableRes iconResId: Int,
+    color: Color,
     statValue: Int,
     statName: String,
 ) {
-    Row(
-        verticalAlignment = Alignment.Bottom,
+    Box(
+        modifier = modifier.then(
+            Modifier.padding(horizontal = 32.dp)
+                .height(96.dp).width(100.dp),
+        ),
+        contentAlignment = Alignment.CenterStart,
     ) {
-        Text(
-            modifier = Modifier.alignByBaseline(),
-            text = "$statValue",
-            style = h1TextStyle,
-        )
-        Text(
-            modifier = Modifier.alignByBaseline(),
-            text = "$statName",
-            style = h3TextStyle,
-        )
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.CenterEnd,
+        ) {
+            Icon(
+                modifier = Modifier.size(96.dp),
+                painter = painterResource(iconResId),
+                contentDescription = null,
+                tint = Color.Black.copy(alpha = .6f),
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Text(
+                text = "$statValue",
+                style = h1TextStyle,
+                color = color,
+            )
+            Text(
+                text = statName,
+                style = h3TextStyle,
+            )
+        }
     }
 }
