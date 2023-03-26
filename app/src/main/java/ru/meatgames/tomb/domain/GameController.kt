9f485e469941d656694178e74daddbd9602d9b02
Maksim Artsybasov <maksim.artsybasov@glovoapp.com>
2023-03-26 12:16:17 +0200

    New map button now generates current map type

--- app/src/main/java/ru/meatgames/tomb/domain/GameController.kt
+++ app/src/main/java/ru/meatgames/tomb/domain/GameController.kt
@@ -8,10 +8,14 @@ class GameController @Inject constructor(
     private val mapCreator: MapCreator,
     private val characterController: CharacterController,
 ) {
+    
+    lateinit var lastMapType: MapCreator.MapType
+        private set
 
     fun generateNewMap(
         mapType: MapCreator.MapType,
     ) {
+        lastMapType = mapType
         val configuration = mapCreator.createNewMap(mapType)
         characterController.setPosition(
             coordinates = configuration.startCoordinates,
