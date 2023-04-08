c785e9bdd33a6745d1ab78e895bfe534f908c1c8
Maksim Artsybasov <maksim.artsybasov@glovoapp.com>
2023-04-08 13:40:50 +0200

    Add EnemyTurnResult.Move

--- app/src/main/java/ru/meatgames/tomb/domain/turn/EnemyTurnResult.kt
+++ app/src/main/java/ru/meatgames/tomb/domain/turn/EnemyTurnResult.kt
@@ -1,12 +1,19 @@
 package ru.meatgames.tomb.domain.turn
 
 import ru.meatgames.tomb.Direction
+import ru.meatgames.tomb.domain.enemy.EnemyId
 
 sealed class EnemyTurnResult {
     
     data class Attack(
+        val enemyId: EnemyId,
         val direction: Direction,
         val amount: Int,
+    ) : EnemyTurnResult()
+    
+    data class Move(
+        val enemyId: EnemyId,
+        val direction: Direction,
     ) : EnemyTurnResult()
     
 }
